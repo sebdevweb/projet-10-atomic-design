56fcf738d52865d70f26982df09876c7f34ac8e4
sebdevweb <spansini@squad.fr>
2024-10-17 15:53:34 +0200

    [ATOMIC-DESIGN-VUE-APP] Add vue-router

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Buttons from "./components/molecules/Buttons.vue";
+import Form from "./components/molecules/Form.vue";
+// import BlogPage from "./pages/BlogPage.vue";
+// import ContactPage from "./pages/ContactPage.vue";
+// import HomePage from "./pages/HomePage.vue";
+// import NotFoundPage from "./pages/NotFoundPage.vue";
+// import SinglePage from "./pages/SinglePage.vue";
+
+export const routes = [
+  {path: '/', component: Buttons},
+  {path: '/input', component: Form}
+]
