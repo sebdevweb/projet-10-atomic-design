542110de6e01d029e5cc311866816782b673a9f5
sebdevweb <spansini@squad.fr>
2024-10-17 10:08:11 +0200

    [ATOMIC-DESIGN-VUE-APP] Add color switcher buttons

--- src/components/molecules/Button.vue
+++ src/components/molecules/Button.vue
@@ -1,14 +1,5 @@
 <template>
-  <button
-    :class="`${className}
-      ${large ? 'large' : ''}
-      ${medium ? 'medium' : ''}
-      ${small ? 'small' : ''}
-      ${primary ? 'primary' : ''}
-      ${secondary ? 'secondary' : ''}
-      ${tertiary ? 'tertiary' : ''}
-    `"
-  >
+  <button :class="`${className}`">
     <component :is="props.leftIcon" class="middle" />
     {{ buttonText }}
     <component :is="props.rightIcon" class="right-middle"/>
@@ -20,14 +11,14 @@
   const props = defineProps({
     buttonText: {
       type: String,
-      required: true,
       default: 'Action button'
     },
     className: {
       type: String,
       required: true
     },
-    leftIcon: Object,
-    rightIcon: Object
+    
+    leftIcon: Function,
+    rightIcon: Function
   })
 </script>
